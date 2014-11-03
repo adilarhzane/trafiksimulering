b394c834f424c79e20720063084d19c38977fd8b
adilarhzane <adilarhzane@gmail.com>
2014-11-03 14:02:27 +0100

    Fixing ReadString

--- suggestion1/ReadString.java
+++ suggestion1/ReadString.java
@@ -1,28 +1,59 @@
 import java.io.*;
 
-
 public class ReadString {
 
     public static void main (String[] args){
 
-	System.out.print("Enter your name: ");
+	System.out.print("Enter arrival intensity: ");
+	BufferedReader intensity = new BufferedReader(new InputStreamReader(System.in));
+	int intensity = 0;
 
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	System.out.print("Enter period time: ");
+	BufferedReader period = new BufferReader(new InputStreamReader(System.in));
+	int period = 0;
+    
+	System.out.print("Enter green light time: ");
+	BufferedReader greenTime = new BufferReader(new InputStreamReader(System.in));
+	int greenTime = 0;
 
-	String userName = null;
+	System.out.print("Enter length of big road: ");
+	BufferedReader r0 = new BufferReader(new InputStreamReader(System.in));
+	int r0 = 0;
 
+	System.out.print("Enter length of turn road ");
+	BufferedReader r2 = new BufferReader(new InputStreamReader(System.in));
+	int r2 = 0;
 
 	try{
 
-	    userName = br.readLine();
-	
+	    intensity = intensity.readLine();
+	    period = period.readLine();
+	    greenTime = greenTime.readLine();
+	    r0 = r0.readLine();
+	    r2 = r2.readLine();
+	   
 	}catch (IOException ioe){
-	    System.out.println("Error reading your name:");
-	    System.exit(1);
 
-	}
+	    System.out.println("Error reading value");
+	}catch (NumberFormatException e){
+	    
+	    System.out.println("Not a digit");
 	
-	System.out.println("Nice name: " + userName);
+	}
     }
-    
+
+    /*
+    public static Boolean convert(String con){
+
+	for(int i = 0; i < con.length(); i++){
+	    
+	    char s = con.charAt(0);
+	    if (!Character.isLetter(s)){
+		return false;
+	    }
+	    
+	}
+	return true;
+	}  */
+  
 }
